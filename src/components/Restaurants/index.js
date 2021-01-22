1441977e1564d8ff8cb6aee7c93e825cdd63b5e9
Lucas Rodrigues <rodrigues14set@hotmail.com>
2021-01-21 21:43:37 -0300

    End Clone Ifood

--- src/components/Restaurants/index.js
+++ src/components/Restaurants/index.js
@@ -34,7 +34,26 @@ export default function Restaurants({ title, display }) {
 
   return (
     <Container>
-     
+      <Header display={display}>
+        <Title>Restaurantes</Title>
+      </Header>
+      
+      <RestaurantList>
+        {restaurants.map(item => (
+          <Item>
+            <ItemImage source={{ uri: item.restaurant_url }} />
+            <ItemInfo>
+              <ItemTitle>{item.title}</ItemTitle>
+              <ItemDescription>
+                <MaterialIcons name="star" size={20} color="#FF6666" />
+                <Star>{ item.star ? item.star : 'Novo!' }</Star>
+                <Distance>{item.distance}</Distance>
+              </ItemDescription>
+              <Delay>{item.time}</Delay>
+            </ItemInfo>
+          </Item>
+        ))}
+      </RestaurantList>
     </Container>
   );
 }
